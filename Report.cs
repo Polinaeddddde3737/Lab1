fcb7fc656faf5b36fb0e1594c0245d5ae68a7c2f
Матзянова <polinahhhdhd@gmail.com>
2026-04-08 22:50:25 +0300

    Матзянова - Добавлен класс Report.cs

---
+++ Report.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+public class Report
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreationDate { get; set; }
+
+    public Report(string title, string content, DateTime creationDate)
+    {
+        Title = title;
+        Content = content;
+        CreationDate = creationDate;
+    }
+}
+
